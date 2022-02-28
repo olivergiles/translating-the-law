72eaec90723e2e65ee30a056bd1ae43ac54a1dad
Oliver Giles <ogiles@email.com>
2022-02-28 17:21:03 +0000

    added way to debug failed parses

--- translating-the-law/downloading/parse.py
+++ translating-the-law/downloading/parse.py
@@ -8,13 +8,15 @@ def summary_to_dict(summary):
     summary_regex = r"PRESS SUMMARY(.*)JUSTICES:(.*)BACKGROUND TO THE APPEAL(.*)JUDGMENT(.*)REASONS FOR THE JUDGMENT(.*)"
     summary = summary.replace("\n", "").strip()
     search = re.search(summary_regex,summary,flags=re.M)
-    return {
-        "Press summary":search.group(1).strip(),
-        "Justices":search.group(2).strip(),
-        "Background to the appeal":search.group(3).strip(),
-        "Judgment":search.group(4).strip(),
-        "Reasons for the judgment":search.group(5).strip()
-    }
+    if search:
+        return {
+            "Press summary":search.group(1).strip(),
+            "Justices":search.group(2).strip(),
+            "Background to the appeal":search.group(3).strip(),
+            "Judgment":search.group(4).strip(),
+            "Reasons for the judgment":search.group(5).strip()
+        }
+    return {"error":"cannot parse"}
 
 def parse_all(judgement, summary):
     j = judgement_to_dict(judgement)
