57d71db76f5cdf5864cddfcd2089ce98f560731a
Oliver Giles <ogiles@email.com>
2022-03-07 16:18:05 +0000

    api function updated

---
+++ question_docker/api/get_from_bucket.py
@@ -0,0 +1,11 @@
+import json
+import gcsfs
+
+def open_from_bucket():
+    gcs_file_system = gcsfs.GCSFileSystem()
+    gcs_json_path = "gs://law-data-ogiles/data/simplified_data.json"
+    with gcs_file_system.open(gcs_json_path) as f:
+        json_dict = json.load(f)
+    data = eval(json_dict)
+    clean_data = [case for case in data if not case["press summary"].get('error')]
+    return clean_data
