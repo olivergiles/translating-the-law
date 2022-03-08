f895cfdc5c3c8c2040b9bc3a4faa2f0e1f8a0201
Kathryn Baldewin <katiebaldewin@gmail.com>
2022-03-08 17:43:00 +0000

    updates to case search page

--- streamlit/pages/case_search.py
+++ streamlit/pages/case_search.py
@@ -7,8 +7,9 @@ def app():
     st.subheader('Search for a case by keyword')
     st.caption("Need to quickly understand a UK Supreme Court case?\
         Search the one you're looking for by name, justice, case ID, or other keyword.")
-    st.text_input('Search for case by keyword')
-    #not sure how to make text in search bar lighter but something to consider
+    query = st.text_input('Search for case by keyword',
+                  placeholder='Enter case detail or topic')
+    st.write('Search results for: ', query)
 
     #st.sidebar.title('Choose a case:')
     #st.sidebar.button('Select case from year')
