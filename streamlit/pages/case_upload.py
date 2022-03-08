df671de17ec6bb331f77ef8b59317fdcdbcbb085
Kathryn Baldewin <katiebaldewin@gmail.com>
2022-03-08 17:43:35 +0000

    text input is interactive now! yay!

--- streamlit/pages/case_upload.py
+++ streamlit/pages/case_upload.py
@@ -7,7 +7,10 @@ def app():
     st.subheader('Upload your own case')
     st.caption("Have a court judgment or press release you want to understand?\
         Enter the text of your document below to have it quickly summarized.")
-    st.text_area('Enter judgment text for summarization')
+    txt = st.text_area('Enter judgment text for summarization',
+                       max_chars=None,
+                       placeholder='Max char: ?')
+    st.write('The text you entered was:', txt)
     st.write("The model-generated summary will show up here")
 
     #st.sidebar.title('Choose a case:')
