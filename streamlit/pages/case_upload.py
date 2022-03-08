14d9f3eb7fa1a3cc26c8a654e718e732d533fb56
Kathryn Baldewin <katiebaldewin@gmail.com>
2022-03-08 18:16:03 +0000

    attempted an api call from this page to moderate success

--- streamlit/pages/case_upload.py
+++ streamlit/pages/case_upload.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 def app():
     st.title('Translate the Law')
@@ -10,10 +11,17 @@ def app():
     txt = st.text_area('Enter judgment text for summarization',
                        max_chars=None,
                        placeholder='Max char: ?')
-    st.write('The text you entered was:', txt)
+    summary = requests.get(f'https://uskc-summarizer-app-jaefennyiq-ew.a.run.app/summary?text={txt}').json()['summary']
+    st.write('The text you entered was:', summary)
     st.write("The model-generated summary will show up here")
 
     #st.sidebar.title('Choose a case:')
     #st.sidebar.button('Select case from year')
     #st.sidebar.button('Search case by keyword')
     #st.sidebar.button('Upload your own text')
+
+
+    #if or def to stop summary from running before text is defined
+    #run it via a button instead of just through the box for clearer results
+    #sometimes after we've been using the site too much, the multiapp nav gets weird
+    #(aka stops working)
