5d8cea022aac024ae3a0b64fddff6e1e84533726
Kathryn Baldewin <katiebaldewin@gmail.com>
2022-03-09 15:38:08 +0000

    not sure why i edited this page, we are not even using it anymore

--- streamlit/pages/case_result.py
+++ streamlit/pages/case_result.py
@@ -5,14 +5,14 @@ import pandas as pd
 questions_df = pd.DataFrame({'questions': ['Please select', 'Sample Question 1', 'Sample Question 2']})
 
 def app():
-    st.header("Bloomberg LP (Appellant) v ZXC (Respondent)")
+    st.header('Bloomberg LP (Appellant) v ZXC (Respondent)')
     st.write('##')
-    st.subheader("Summary")
-    st.write("The model-generated summary will show up here,\
+    st.subheader('Summary')
+    st.write('The model-generated summary will show up here,\
             along with some of the other relevant case details such as case id number,\
-            judgment date, names of justices, and neutral citation number(?)")
+            judgment date, names of justices, and neutral citation number(?)')
     st.write('##')
-    st.subheader("Q&A")
+    st.subheader('Q&A')
     col1, col2 = st.columns([2,3])
     with col1:
         our_q = st.selectbox('Suggested questions',
