1b98ca39e537413429bb9b73fbcc05c4f94e2436
Oliver Giles <ogiles@email.com>
2022-03-14 10:19:08 +0000

    final cleanup

--- full_app/streamlit/pages/graph_viz.py
+++ full_app/streamlit/pages/graph_viz.py
@@ -3,10 +3,11 @@ import pandas as pd
 import plotly.express as px
 
 
-clustering_data = st.cache(pd.read_csv('graph_viz.csv'))
+
 
 def app():
     button = st.button('Launch cluster')
     if button:
+        clustering_data = pd.read_csv('graph_viz.csv')
         fig = px.scatter_3d(clustering_data, x='x', y='y', z='z',color = clustering_data['labels'])
         st.plotly_chart(fig)
